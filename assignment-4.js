b50521e0362d91c590c25603800b9d95a993054f
MAINUL ISLAM <mainulislam6168@gmail.com>
2023-04-09 22:57:44 +0600

    completed assignment

--- assignment-4.js
+++ assignment-4.js
@@ -37,3 +37,38 @@ function isLGSeven(number) {
     }
     else { return number * 2 };
 }
+
+/* problem -4: (Finding Bad data) take an array as input. if any number is less than zero, it is 'bad data'. and if number is greater than zero, it is 'good data'.Now I have to return how many 'bad data is there */
+
+let myArray = [7, 21, -4, -9, 10, -99, -32,];
+
+function findingBadData(myArray) {
+    let badNumbers = [];
+
+    for (let i = 0; i < myArray.length; i++) {
+        if (myArray[i] < 0) {
+            badNumbers.push(myArray[i]);
+        }
+    }
+    return badNumbers.length;
+}
+
+/* problem-5 :(convert gems to diamond) take three friends gems number as 3 input parameter. three friends gems power is such as 21, 32, 43. now find out how many diamond will get from this tree friends gems. if diamond number is more than 1000*2, subtract 2000 from total diamond. and that will be final result.*/
+
+function gemsToDiamond(friend1Gems, friend2Gems, friend3Gems) {
+    if (typeof friend1Gems !== 'number' || typeof friend2Gems !== 'number' || typeof friend3Gems !== 'number') {
+        return 'Input should be number'
+    };
+    const friend1GemPower = 21;
+    const friend2GemPower = 32;
+    const friend3GemPower = 43;
+
+    const friend1Diamond = friend1Gems * friend1GemPower;
+    const friend2Diamond = friend2Gems * friend2GemPower;
+    const friend3Diamond = friend3Gems * friend3GemPower;
+    const totalDiamond = friend1Diamond + friend2Diamond + friend3Diamond;
+
+    if (totalDiamond > 1000 * 2) { return totalDiamond - 2000 }
+    else { return totalDiamond };
+
+}
