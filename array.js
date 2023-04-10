72ec3695462208d56b168dc3a18a04a7168f804a
MAINUL ISLAM <mainulislam6168@gmail.com>
2023-04-10 09:56:31 +0600

    array details

---
+++ array.js
@@ -0,0 +1,15 @@
+// array
+const product = [{name: 'phone', price:21000},
+{name: 'laptop', price:32000},
+{name: 'head phone', price:2000},
+]
+// console.log(product);
+// console.log(product[0])
+
+// object like array
+const products = {
+    '0':12,
+    '1': 32,
+    '3':22
+}
+console.log(Array.isArray(products));
